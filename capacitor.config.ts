ee7d31c1984c84fa7c409f3e2629818c98eb4c1b
fabwedding <akansha@fabweddings.in>
2022-05-03 17:11:40 +0530

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adorta.madarskom',
+  appName: 'Madarskom',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
